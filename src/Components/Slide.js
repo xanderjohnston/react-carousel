96bce2e1a8203b775273b57b6cc3b06bfe241e59
Xander Johnston <alexjohnston@ymail.com>
2019-10-23 17:33:59 +0100

    refactored transform for readability

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Slide = ({ img, title, copy, scaleDown, percentage, checkHeight, selected }) => {
 
+  const transformStyle = {
+    transform: `translateX(${percentage}%) scale(${scaleDown ? 0.7 : 1})`
+  }
+
   return  <div ref={selected ? checkHeight : null} 
             className={"slide-image"} 
-            style={{transform: `translateX(${percentage}%) scale(${scaleDown ? 0.7 : 1})`}} >
+            style={transformStyle} >
             <img src={img} style={{width: "100%"}}/>
-              <div className={"slide-text"} style={{transform: `translateX(${percentage}%) scale(${scaleDown ? 0.7 : 1})`}} >
+              <div className={"slide-text"} style={transformStyle} >
                 <h3 >{title}</h3>
                 <p >{copy}</p>
               </div>
