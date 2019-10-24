af910c65d392de6c9d6838e9c58776d9a77fcb7d
Xander Johnston <alexjohnston@ymail.com>
2019-10-24 12:15:02 +0100

    styled arrow buttons

--- src/Components/ArrowButton.js
+++ src/Components/ArrowButton.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ArrowButton = ({ direction, handleClick }) => {
 
-  return <div className={`slide-arrow ${direction} fade-in`} onClick={() => handleClick()}>
+  return  <div className={`slide-arrow ${direction} fade-in`} onClick={() => handleClick(direction)}>
             <i className={`fas fa-chevron-circle-${direction}`} />
-        </div>
+          </div>
 }
 
 export default ArrowButton
