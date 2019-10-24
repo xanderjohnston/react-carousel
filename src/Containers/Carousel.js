e3b759d398ef447ca59a4146ed5659b7b19d8b04
Xander Johnston <alexjohnston@ymail.com>
2019-10-24 12:15:31 +0100

    separation of text and image logic for carousel and refactoring

--- src/Containers/Carousel.js
+++ src/Containers/Carousel.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import Slide from '../Components/Slide'
+import Image from '../Components/Image'
+import Text from '../Components/Text'
 import ArrowButton from '../Components/ArrowButton'
 
 const Carousel = ({slides}) => {
@@ -7,62 +8,70 @@ const Carousel = ({slides}) => {
     const [canRight, setRight] = useState(true)
     const [canLeft, setLeft] = useState(false)
     const [percentage, setPercentage] = useState(0)
-    const [height, setHeight] = useState(0);
-    const [width, setWidth] = useState(0);
-
-    const measuredRef = useCallback(node => {
+    const [imageHeight, setImageHeight] = useState(0);
+    const [imageWidth, setImageWidth] = useState(0);
+    const [textHeight, setTextHeight] = useState(0);
+    
+    const imageRef = useCallback(node => {
       if (node) {
-        setHeight(node.scrollHeight);
-        setWidth(node.clientWidth)
+        setImageHeight(node.scrollHeight);
+        setImageWidth(node.clientWidth)
       }
     }, []);
 
-    const handleRightClick = () => {
-        if (canRight) {
-          setCurrent(currentSlide + 1)
-        }
-        setPercentage(percentage - 100)
-    }
+    const textRef = useCallback(node => {
+      if (node) {
+        setTextHeight(node.scrollHeight);
+      }
+    }, []);
 
-    const handleLeftClick = () => {
-        if (canLeft) {
-          setCurrent(currentSlide - 1)
-        }
+    const handleClick = (direction) => {
+      if (direction === "right") {
+         setCurrent(currentSlide + 1)
+         setPercentage(percentage - 100)
+      } else {
+        setCurrent(currentSlide - 1)
         setPercentage(percentage + 100)
-    }
-
-    const findScale = (index) => {
-      return Math.abs(currentSlide - index)
+      }
     }
 
     useEffect(() => {
-        if (currentSlide > 0) {
-            setLeft(true)
-        } else if (currentSlide === 0) {
-            setLeft(false)
-        }
-        if (currentSlide >= (slides.length - 1)) {
-            setRight(false)
+        if (currentSlide === 0) {
+          setLeft(false)
+        } else if (currentSlide >= (slides.length - 1)) {
+          setRight(false)
         } else {
-            setRight(true)
+          setLeft(true)
+          setRight(true)
         }
-    }, [currentSlide])
+      }, [currentSlide])
+
         
     return  <div className={"carousel"} >
-                <div className={"slides"} style={{minHeight: `${height}px`}}>
-                  {slides.map((slide, i) => {
-                    return  <Slide  {...slide} 
+                <div className={"slides"} >
+                  <div className={"image-wrap"} style={{height: `${imageHeight}px`}} >
+                    {slides.map((slide, i) => {
+                    return  <Image  {...slide} 
                               selected={currentSlide === i}
-                              checkHeight={measuredRef}
-                              scaleDown={findScale(i)}
+                              checkHeight={imageRef}
+                              percentage={(percentage + (i*100))}
+                            />
+                            })}
+                      <div className={"arrows-container"} style={{minWidth: `${imageWidth + 15}px`}}>
+                        {canLeft && <ArrowButton direction={"left"} handleClick={handleClick} />}
+                        {canRight && <ArrowButton direction={"right"} handleClick={handleClick} />}
+                      </div>
+                  </div>
+                  <div className={"text-wrap"} style={{height: `${textHeight}px`}}>
+                  {slides.map((slide, i) => {
+                    return  <Text  {...slide} 
+                              selected={currentSlide === i}
+                              checkHeight={textRef}
                               percentage={(percentage + (i*100))}
                             />
                     })}
+                  </div>
                 </div>
-                <div className={"arrows-container"} style={{minWidth: `${width + 15}px`}}>
-                      {canLeft && <ArrowButton direction={"left"} handleClick={handleLeftClick} />}
-                      {canRight && <ArrowButton direction={"right"} handleClick={handleRightClick} />}
-                    </div>
             </div>
     
 }
